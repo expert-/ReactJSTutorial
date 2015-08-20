2d9c09e6a7019a3229009cd59ec2a4b877f84f0f
Ayush <ayush15may@gmail.com>
2015-08-20 22:28:15 +0530

    jsx transformed into js and used
    works fine for all browsers

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,4 +1,4 @@
 React.render(
-  <h1>Hello, world! this jsx is giving problem in chrome and some versions of IE but works fine in Firefox</h1>,
+  React.createElement('h1', null, 'Hello, world!'),
   document.getElementById('example')
 );
